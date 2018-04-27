ef86f38f9610310b50993e40ca670d0b6d8bea37
Kamil <kamil@altel.pl>
2018-04-27 16:00:23 +0200

    Startowy, sciagnieta biblioteka nugetem

---
+++ GoogleDriveApiTests/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+// usingi
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+/*
+
+	NUGET
+	Install-Package Google.Apis.Drive.v3
+
+ */
+
+
+namespace GoogleDriveApiTests
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Start_Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
